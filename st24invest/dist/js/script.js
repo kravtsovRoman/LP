7e337b54d1815e3f35efcaac63ccad51e619c7ea
kravtsovRoman <rozario91208@gmail.com>
2018-05-23 11:36:21 +0300

    news page

--- st24invest/dist/js/script.js
+++ st24invest/dist/js/script.js
@@ -22,4 +22,13 @@ $(document).ready(function(){
   //   event.stopPropagation();
   // });
 
+// Facebook comments
+  (function(d, s, id) {
+    var js, fjs = d.getElementsByTagName(s)[0];
+    if (d.getElementById(id)) return;
+    js = d.createElement(s); js.id = id;
+    js.src = 'https://connect.facebook.net/ru_RU/sdk.js#xfbml=1&version=v3.0';
+    fjs.parentNode.insertBefore(js, fjs);
+  }(document, 'script', 'facebook-jssdk'));
+
 
