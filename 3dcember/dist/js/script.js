63787964a253b7d05393716d9b055b76f565437d
kravtsovRoman <rozario91208@gmail.com>
2018-05-25 17:58:36 +0300

    start 3dcember

---
+++ 3dcember/dist/js/script.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+
+
+  // $(window).scroll(function() {
+  //   if ($(this).scrollTop() > 25) {
+  //     $(".out-list li").addClass("menu-padding");
+  //     $(".header").addClass("header-padding");
+  //     $(".header-top").addClass("header-top-padding");
+  //   }
+  //   else{
+  //     $(".out-list li").removeClass("menu-padding");
+  //     $(".header").removeClass("header-padding");   
+  //     $(".header-top").removeClass("header-top-padding");     
+  //   }
+  // });
+});
+
+
+  //Burger menu в моб. версии
+  // $('#burger_check+label').on('click', function(event){
+  //   $('.nav-menu').fadeToggle(200);
+  //   event.stopPropagation();
+  // });
